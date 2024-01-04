2acc03e331ed7a803b4c447de227dc2a6abd4dd6
Govind S Kumar <govindsajeev2001@gmail.com>
2024-01-04 12:09:16 +0530

    commit

---
+++ src/store.jsx
@@ -0,0 +1,20 @@
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import { thunk } from "redux-thunk";
+import { accountReducer} from "./components/features/account/accountSlice";
+import {customerReducer} from './components/features/customer/customerSlice'
+
+
+import { composeWithDevTools } from "@redux-devtools/extension";
+
+
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
+});
+
+
+export const store = createStore(rootReducer,
+ composeWithDevTools( 
+  applyMiddleware(thunk))
+  );
+
